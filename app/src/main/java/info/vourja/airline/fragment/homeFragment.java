e84e7846beca3f68a79369a418a8837cc1a28968
YukawaJunichi <vourja@YukawaJunichi-no-MacBook-Pro.local>
2016-06-22 01:56:56 +0900

    #3 Activityのリストを取得する処理記述

--- app/src/main/java/info/vourja/airline/fragment/homeFragment.java
+++ app/src/main/java/info/vourja/airline/fragment/homeFragment.java
@@ -7,7 +7,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+
+import java.util.List;
+
+import info.vourja.airline.AirLineApplication;
+import info.vourja.airline.Model.AirlineActivity;
+import info.vourja.airline.NetService.AirLineService;
+import info.vourja.airline.NetService.util;
 import info.vourja.airline.R;
 
 public class HomeFragment extends Fragment {
@@ -24,5 +36,28 @@ public class HomeFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_home, container, false);
 
         return v;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        AirLineApplication application = (AirLineApplication)getActivity().getApplication();
+        TwitterSession session = application.getTwitterSession();
+        AirLineService service = application.getAirlineService();
+        String token_secret = util.getCredentials(session.getAuthToken().token, session.getAuthToken().secret);
+
+        service.getActivities(token_secret, new Callback<List<AirlineActivity>>() {
+            @Override
+            public void success(Result<List<AirlineActivity>> result) {
+
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+
+            }
+        });
+
     }
 }
