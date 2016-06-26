9fb30e313fae011935e1d867e205b8fae067ab3c
YukawaJunichi <vourja@YukawaJunichi-no-MacBook-Pro.local>
2016-06-27 03:14:31 +0900

    いろいろ修正

--- app/src/main/java/info/vourja/airline/Model/Line.java
+++ app/src/main/java/info/vourja/airline/Model/Line.java
@@ -3,14 +3,44 @@ package info.vourja.airline.Model;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import java.util.UUID;
+
 /**
  * Created by vourja on 2016/06/22.
  */
 public class Line {
 
     @Expose
+    @SerializedName("uuid")
+    private UUID uuid;
+
+    @Expose
     @SerializedName("customer_id")
     private int customer_id;
+
+    @Expose
+    @SerializedName("number")
+    private int number;
+
+    @Expose
+    @SerializedName("create_date")
+    private String create_date;
+
+    @Expose
+    @SerializedName("arrived_date")
+    private String arrived_date;
+
+    @Expose
+    @SerializedName("pass_date")
+    private String pass_date;
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
 
     public int getCustomer_id() {
         return customer_id;
@@ -18,5 +48,37 @@ public class Line {
 
     public void setCustomer_id(int customer_id) {
         this.customer_id = customer_id;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getCreate_date() {
+        return create_date;
+    }
+
+    public void setCreate_date(String create_date) {
+        this.create_date = create_date;
+    }
+
+    public String getArrived_date() {
+        return arrived_date;
+    }
+
+    public void setArrived_date(String arrived_date) {
+        this.arrived_date = arrived_date;
+    }
+
+    public String getPass_date() {
+        return pass_date;
+    }
+
+    public void setPass_date(String pass_date) {
+        this.pass_date = pass_date;
     }
 }
--- app/src/main/java/info/vourja/airline/activity/LineActivity.java
+++ app/src/main/java/info/vourja/airline/activity/LineActivity.java
@@ -1,21 +1,45 @@
 package info.vourja.airline.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import info.vourja.airline.AirLineApplication;
+import info.vourja.airline.Model.AirLineActivity;
+import info.vourja.airline.NetService.AirLineService;
+import info.vourja.airline.NetService.util;
 import info.vourja.airline.R;
 import info.vourja.airline.fragment.HomeFragment;
 import info.vourja.airline.fragment.LineFragment;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class LineActivity extends AppCompatActivity {
+
+    private String mUUID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_line);
+
+        if(savedInstanceState != null) {
+            mUUID = savedInstanceState.getString("uuid");
+        } else {
+            Intent i = getIntent();
+            mUUID = i.getStringExtra("uuid");
+        }
 
         // 時間表示したら面白いかも
         setTitle(R.string.main_title);
@@ -23,10 +47,65 @@ public class LineActivity extends AppCompatActivity {
         setupFragment();
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("uuid", mUUID);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        MenuItem item = menu.add(R.string.action_finish);
+        item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                // おしまい
+                AirLineApplication application = (AirLineApplication)getApplication();
+                String token = application.getAccessToken();
+                AirLineService service = application.getAirlineService();
+                String token_secret = util.getCredentials(token, "unused");
+
+                Call<AirLineActivity> call = service.finishActivity(token_secret, mUUID);
+                call.enqueue(new Callback<AirLineActivity>() {
+                    @Override
+                    public void onResponse(Call<AirLineActivity> call, Response<AirLineActivity> response) {
+                        if(response.code() == 200) {
+                            finish();
+                        } else {
+                            // TODO 終了するのに失敗したぞ、どうしたらいいんだ
+                        }
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<AirLineActivity> call, Throwable t) {
+                        // TODO 終了するのに失敗したぞ、どうしたらいいんだ
+                    }
+                });
+
+                return false;
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (scanResult != null) {
+            String uuid = scanResult.getContents();
+        }
+    }
+
     private void setupFragment() {
         FragmentManager manager = getSupportFragmentManager();
         LineFragment lineFragment = new LineFragment();
         Bundle bundle = new Bundle();
+        bundle.putString("uuid", mUUID);
         lineFragment.setArguments(bundle);
 
         FragmentTransaction tr = manager.beginTransaction();
